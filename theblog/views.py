c9d6ce1eee089b73f6eabd6bd930b3d82c715de5
jm_hasan@yahoo.com <jm_hasan@yahoo.com>
2021-01-19 00:32:28 +0600

    first commit

---
+++ theblog/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+from django.views.generic import ListView, DetailView
+from .models import Post
+
+#def home(request):
+    #return render(request, 'home.html')
+
+
+
+class HomeView(ListView):
+    model = Post
+    template_name = 'home.html'
+
+
+class ArticalDetailView(DetailView):
+    model = Post
+    template_name = 'arical_details.html'
