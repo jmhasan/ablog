73171be1b41f56f555cd8115dc613c7fce3789e5
jm_hasan@yahoo.com <jm_hasan@yahoo.com>
2021-01-21 23:37:53 +0600

    error fix

--- theblog/models.py
+++ theblog/models.py
@@ -28,4 +28,4 @@ class Post(models.Model):
         return self.title + '|' + str(self.author)
 
     def get_absolute_url(self):
-        return reverse('article-dtail', args=(str(self.id)))
+        return reverse('home')
